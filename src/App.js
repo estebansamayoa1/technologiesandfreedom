e5d7651e1ba3ec5914b3297abd863faaea540b41
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2023-09-22 15:59:34 -0600

    Arreglos al frontend (final)

--- src/App.js
+++ src/App.js
@@ -12,26 +12,17 @@ const App = () => {
   const [icoStatus, setICOStatus] = useState({});
   const [amount, setAmount] = useState(0);
 
-
+  const contractTokenAddress = '0xBbF8D75d95409e70369D4BC4273ff28D0463D4e7';
 
   useEffect(() => {
     async function loadWeb3() {
       if (typeof window !== 'undefined' && window.ethereum) {
         const web3Instance = new Web3(window.ethereum);
         try {
-          // Request user permission to connect
           await window.ethereum.enable();
-          
-          // Get accounts
+          setWeb3(web3Instance);
           const accounts = await web3Instance.eth.getAccounts();
-  
-          if (accounts.length > 0) {
-            // Account available
-            setWeb3(web3Instance);
-            setAccount(accounts[0]);
-          } else {
-            console.error("No Ethereum accounts available.");
-          }
+          setAccount(accounts[0]);
         } catch (error) {
           console.error("Error while enabling MetaMask:", error);
         }
@@ -39,44 +30,51 @@ const App = () => {
         console.error("MetaMask not detected.");
       }
     }
-  
+
     loadWeb3();
   }, []);
   
-
   useEffect(() => {
     if (web3) {
-      // Load Ethereum wallet balance
       const loadEthBalance = async () => {
         const balanceWei = await web3.eth.getBalance(account);
         const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
         setSepoliaETHBalance(balanceEth);
+        const contractTokenContract = new web3.eth.Contract(contractABI, contractTokenAddress);
+        const contractTokenBalanceWei = await contractTokenContract.methods.balanceOf(account).call();
+        const contractTokenBalance = web3.utils.fromWei(contractTokenBalanceWei, 'ether');
+        setVandidocoinBalance(contractTokenBalance);
       };
 
       loadEthBalance();
     }
   }, [web3, account]);
 
-  async function investInICO() {
+  const investInICO = async () => {
     try {
       const investmentInEther = parseFloat(amount);
       if (isNaN(investmentInEther) || investmentInEther <= 0) {
         alert('Ingrese una cantidad válida para invertir.');
         return;
       }
-  
-      const investmentInWei = web3.utils.toWei(investmentInEther, 'ether');
-  
-      const contract = new web3.eth.Contract(contractABI, 0xECB13Cf26CA05A4C65cb8bdA5230036adc52A2BA);
-      await contract.methods.invest().send({
+
+      const investmentInWei = web3.utils.toWei(investmentInEther.toString(), 'ether');
+      const contract = new web3.eth.Contract(contractABI, contractTokenAddress);
+
+      await contract.methods.claimTokens().send({
         from: account,
         value: investmentInWei,
       });
-  
-    
+
       const balanceWei = await web3.eth.getBalance(account);
       const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
       setSepoliaETHBalance(balanceEth);
+
+      // Create an instance of the contract token
+      const contractTokenContract = new web3.eth.Contract(contractABI, contractTokenAddress);
+      const contractTokenBalanceWei = await contractTokenContract.methods.balanceOf(account).call();
+      const contractTokenBalance = web3.utils.fromWei(contractTokenBalanceWei, 'ether');
+      setVandidocoinBalance(contractTokenBalance);
 
     } catch (error) {
       console.error('Error al invertir:', error);
@@ -92,41 +90,33 @@ const App = () => {
     <div className="center-container">
       <h1>VANDIDOCOIN INTERFACE</h1>
       <div className="box">
-      <h4>Current Wallet: {account}</h4>
-      <div>
-        <p>Sepolia-ETH Balance: {sepoliaETHBalance} ETH</p>
-        <p>Vandidocoin Balance: {vandidocoinBalance} Vandidocoin</p>
-        <p>Pending Claim: {pendingClaim} Vandidocoin</p>
-        <p>ICO Status:</p>
-        {/* Display ICO status details */}
-      </div>
-      <div>
-        <h2>Invest in ICO</h2>
-        <input
+        <h4>Current Wallet: {account}</h4>
+        <div>
+          <p>Sepolia-ETH Balance: {sepoliaETHBalance} ETH</p>
+          <p>Vandidocoin Balance: {vandidocoinBalance} Vandidocoin</p>
+          <p>Pending Claim: {pendingClaim} Vandidocoin</p>
+          <p>ICO Status:</p>
+          {/* Display ICO status details */}
+        </div>
+        <div>
+          <h2>Invest in ICO</h2>
+          <input
             type="text"
             id="investment-amount"
             placeholder="Ej. 2.00"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
           />
-        <button onClick={() => investInICO()} className="btn btn-success">Invest</button>
-        <p><strong>1 SEPOLIA = 90 VANDIDOCOINS</strong></p>
-      </div>
-      <div> 
-        <h2>Claim Vandidocoin</h2>
-        <button onClick={claimVandidocoin}>Claim</button>
-      </div>
+          <button onClick={investInICO} className="btn btn-success">Invest</button>
+          <p><strong>1 SEPOLIA = 90 VANDIDOCOINS</strong></p>
+        </div>
+        <div> 
+          <h2>Claim Vandidocoin</h2>
+          <button onClick={claimVandidocoin}>Claim</button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default App;
-
-
-
-
-
-
-
-
